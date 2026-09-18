f42560df5a94c06aaab4db6003b8537e3bd28d87
Madie <madeleinne.tan@gmail.com>
2026-09-18 13:38:45 -0700

    My brain is not in a coding space right now huhuuu

---
+++ popup.js
@@ -0,0 +1,16 @@
+// Grab the checkbox element by id
+const toggleSwitch = document.getElementById('toggleSwitch');
+
+// On load: 
+
+
+// await chrome.storage.local.get(...), then set checkbox.checked based on truthiness
+
+// Add a change event listener to the checkbox
+
+// Inside that listener: chrome.storage.local.set(...) with the checkbox's new checked value
+
+
+
+
+
