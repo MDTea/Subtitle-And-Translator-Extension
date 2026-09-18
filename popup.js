9bc85501241ba45046468c9029f7ed4ba152d863
Madie <madeleinne.tan@gmail.com>
2026-09-18 16:43:49 -0700

    Saving...

--- popup.js
+++ popup.js
@@ -1,16 +1,24 @@
 // Grab the checkbox element by id
 const toggleSwitch = document.getElementById('toggleSwitch');
 
-// On load: 
+// Function for on-load read 
+async function toggleStateRead() {
+    const result = await chrome.storage.local.get('toggleSwitchState');
+    toggleSwitch.checked = result.toggleSwitchState || false;
+}
 
-
-// await chrome.storage.local.get(...), then set checkbox.checked based on truthiness
-
-// Add a change event listener to the checkbox
-
+// Function for change will be called toggleStateWrite
 // Inside that listener: chrome.storage.local.set(...) with the checkbox's new checked value
+async function toggleStateWrite() {
+    await chrome.storage.local.set({ toggleSwitchState: toggleSwitch.checked });
+}
 
+// Call toggleStateRead on load (immediately)
+// We can trust that the DOM is already loaded because this script is included at the end of the body in popup.html
+toggleStateRead();
 
-
+// Add event listener for change
+// using a function reference instead of a function call so that the function is only called when the event occurs. function() calls it immediately
+toggleSwitch.addEventListener('change', toggleStateWrite);
 
 
